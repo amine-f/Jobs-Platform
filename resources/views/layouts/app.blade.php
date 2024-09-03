d4075fe56047b16cb20c21ad5067c251657be041
Amine (AF) <amineferjani11@gmail.com>
2024-09-03 12:34:40 +0100

    rr

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name')?? 'JobLister' }}</title>
+    <title>{{ config('app.name')?? 'Jobs-platform' }}</title>
 
     <!-- Scripts -->
 
@@ -16,17 +16,17 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
 
     <!-- Styles -->
-    
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">    
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @stack('css')
-    
+
 </head>
 <body>
     <div id="app">
        @yield('layout-holder')
     </div>
-    @include('sweetalert::alert')
+    <!--@include('sweetalert::alert')-->
     <script src="{{ asset('js/app.js') }}"></script>
     @stack('js')
 </body>
