0649440d74a5209f3fabee00b267504122e9e7a2
Amine (AF) <amineferjani11@gmail.com>
2024-09-03 21:02:36 +0100

    the AppServiceProvider.php updated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if (env('APP_ENV') !== 'production') {
+        if (env('APP_ENV') !== 'local') {
                URL::forceScheme('https');
         }
     }
